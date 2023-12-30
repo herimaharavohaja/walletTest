ff214826bbcba9c0ab9cad4783b29fc2e9c3af04
herimaharavohaja <hei.haja.25@gmail.com>
2023-12-30 09:10:47 +0100

    add balance

--- src/main/java/com/wallet/Haja/repository/BalanceRepository.java
+++ src/main/java/com/wallet/Haja/repository/BalanceRepository.java
@@ -1,4 +1,168 @@
 package com.wallet.Haja.repository;
 
-public class BalanceRepository {
+import com.wallet.Haja.config.Connectiondb;
+import com.wallet.Haja.entity.Account;
+import com.wallet.Haja.entity.Balance;
+import com.wallet.Haja.entity.Currency;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class BalanceRepository implements CrudOperations<Balance, Long>{
+    private Connection connection;
+    private PreparedStatement pstmt;
+    private ResultSet rs;
+    private AccountRepository accountRepository;
+
+    @Override
+    public Balance findById(Long id) {
+        try {
+            String SQL = "SELECT * FROM balance WHERE balance_id = ?";
+            connection = Connectiondb.getConnection();
+            pstmt = connection.prepareStatement(SQL);
+            pstmt.setLong(1, id);
+            rs = pstmt.executeQuery();
+            if (rs.next()) {
+                Balance balance = new Balance();
+                balance.setBalanceId(rs.getLong("balance_id"));
+                balance.setBalanceDateTime(rs.getTimestamp("balance_datetime").toLocalDateTime());
+                balance.setAmount(rs.getDouble("amount"));
+                long currencyId = rs.getLong("currency_id");
+                if (currencyId != 0L) {
+                    Account account = this.accountRepository.findById(currencyId);
+                    balance.setAccount(account);
+                }
+                return balance;
+            }
+            throw new RuntimeException("Balance not found with ID: " + id);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            closeResources();
+        }
+    }
+
+    @Override
+    public List<Balance> findAll() {
+        List<Balance> balances = new ArrayList<>();
+        try {
+            String SQL = "SELECT * FROM balance";
+            connection = Connectiondb.getConnection();
+            pstmt = connection.prepareStatement(SQL);
+            rs = pstmt.executeQuery();
+            while (rs.next()) {
+                Balance balance = new Balance();
+                balance.setBalanceId(rs.getLong("balance_id"));
+                balance.setBalanceDateTime(rs.getTimestamp("balance_datetime").toLocalDateTime());
+                balance.setAmount(rs.getDouble("amount"));
+                long accountId = rs.getLong("account_id");
+                if (accountId != 0L) {
+                    Account account = this.accountRepository.findById(accountId);
+                    balance.setAccount(account);
+                }
+                balances.add(balance);
+            }
+            return balances;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            closeResources();
+        }
+    }
+
+
+
+
+    @Override
+    public List<Balance> saveAll(List<Balance> toSave) {
+        List<Balance> savedBalances = new ArrayList<>();
+        try {
+            connection = Connectiondb.getConnection();
+            for (Balance balance : toSave) {
+                String SQL = "INSERT INTO balance (balance_datetime, amount, account_id) VALUES (?, ?, ?)";
+                pstmt = connection.prepareStatement(SQL, Statement.RETURN_GENERATED_KEYS);
+                pstmt.setTimestamp(1, Timestamp.valueOf(balance.getBalanceDateTime()));
+                pstmt.setDouble(2, balance.getAmount());
+                pstmt.setLong(3, balance.getAccount().getAccountId());
+
+                int rowsAffected = pstmt.executeUpdate();
+                if (rowsAffected > 0) {
+                    ResultSet generatedKeys = pstmt.getGeneratedKeys();
+                    if (generatedKeys.next()) {
+                        balance.setBalanceId(generatedKeys.getLong(1));
+                        savedBalances.add(balance);
+                    }
+                }
+            }
+            return savedBalances;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            closeResources();
+        }
+    }
+
+    @Override
+    public Balance save(Balance toSave) {
+        try {
+            connection = Connectiondb.getConnection();
+            String SQL = "INSERT INTO balance (balance_datetime, amount, account_id) VALUES (?, ?, ?)";
+            pstmt = connection.prepareStatement(SQL, Statement.RETURN_GENERATED_KEYS);
+            pstmt.setTimestamp(1, Timestamp.valueOf(toSave.getBalanceDateTime()));
+            pstmt.setDouble(2, toSave.getAmount());
+            pstmt.setLong(3, toSave.getAccount().getAccountId());
+
+            int rowsAffected = pstmt.executeUpdate();
+            if (rowsAffected > 0) {
+                ResultSet generatedKeys = pstmt.getGeneratedKeys();
+                if (generatedKeys.next()) {
+                    toSave.setBalanceId(generatedKeys.getLong(1));
+                    return toSave;
+                }
+            }
+            throw new RuntimeException("Failed to save balance");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            closeResources();
+        }
+    }
+
+
+    @Override
+    public Balance delete(Balance toDelete) {
+        try {
+            String SQL = "DELETE FROM balance WHERE balance_id = ?";
+            connection = Connectiondb.getConnection();
+            pstmt = connection.prepareStatement(SQL);
+            pstmt.setLong(1, toDelete.getBalanceId());
+
+            int rowsAffected = pstmt.executeUpdate();
+            if (rowsAffected > 0) {
+                return toDelete;
+            }
+            throw new RuntimeException("Failed to delete balance with ID: " + toDelete.getBalanceId());
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            closeResources();
+        }
+    }
+
+    private void closeResources() {
+        try {
+            if (rs != null) {
+                rs.close();
+            }
+            if (pstmt != null) {
+                pstmt.close();
+            }
+            if (connection != null) {
+                connection.close();
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
--- src/main/java/com/wallet/Haja/repository/CurrencyRepository.java
+++ src/main/java/com/wallet/Haja/repository/CurrencyRepository.java
@@ -1,15 +1,10 @@
 package com.wallet.Haja.repository;
 
 import com.wallet.Haja.config.Connectiondb;
-import com.wallet.Haja.entity.Account;
-import com.wallet.Haja.entity.AccountType;
 import com.wallet.Haja.entity.Currency;
-import com.wallet.Haja.entity.CurrencyValue;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class CurrencyRepository  implements CrudOperations<Currency, Long> {
@@ -49,21 +44,118 @@ public class CurrencyRepository  implements CrudOperations<Currency, Long> {
 
     @Override
     public List<Currency> findAll() {
-        return null;
+        List<Currency> currencies = new ArrayList<>();
+        try {
+            String SQL = "SELECT * FROM currency";
+            connection = Connectiondb.getConnection();
+            pstmt = connection.prepareStatement(SQL);
+            rs = pstmt.executeQuery();
+            while (rs.next()) {
+                Currency currency = new Currency();
+                currency.setCurrencyId(rs.getLong("currency_id"));
+                currency.setCurrencyName(rs.getString("currency_name"));
+                currency.setCurrencyCode(rs.getString("currency_code"));
+                currencies.add(currency);
+            }
+            return currencies;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            if (pstmt != null || connection != null) {
+                try {
+                    pstmt.close();
+                    connection.close();
+                } catch (SQLException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+
+        }
     }
 
     @Override
     public List<Currency> saveAll(List<Currency> toSave) {
-        return null;
+        List<Currency> savedCurrencies = new ArrayList<>();
+        try {
+            connection = Connectiondb.getConnection();
+            for (Currency currency : toSave) {
+                String SQL = "INSERT INTO currency (currency_name, currency_code) VALUES (?, ?)";
+                pstmt = connection.prepareStatement(SQL, Statement.RETURN_GENERATED_KEYS);
+                pstmt.setString(1, currency.getCurrencyName());
+                pstmt.setString(2, currency.getCurrencyCode());
+                int rowsAffected = pstmt.executeUpdate();
+                if (rowsAffected > 0) {
+                    ResultSet generatedKeys = pstmt.getGeneratedKeys();
+                    if (generatedKeys.next()) {
+                        currency.setCurrencyId(generatedKeys.getLong(1));
+                        savedCurrencies.add(currency);
+                    }
+                }
+            }
+            return savedCurrencies;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            closeResources();
+        }
     }
 
     @Override
     public Currency save(Currency toSave) {
-        return null;
+        try {
+            connection = Connectiondb.getConnection();
+            String SQL = "INSERT INTO currency (currency_name, currency_code) VALUES (?, ?)";
+            pstmt = connection.prepareStatement(SQL, Statement.RETURN_GENERATED_KEYS);
+            pstmt.setString(1, toSave.getCurrencyName());
+            pstmt.setString(2, toSave.getCurrencyCode());
+            int rowsAffected = pstmt.executeUpdate();
+            if (rowsAffected > 0) {
+                ResultSet generatedKeys = pstmt.getGeneratedKeys();
+                if (generatedKeys.next()) {
+                    toSave.setCurrencyId(generatedKeys.getLong(1));
+                    return toSave;
+                }
+            }
+            throw new RuntimeException("Failed to save currency: " + toSave.getCurrencyName());
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            closeResources();
+        }
     }
 
     @Override
     public Currency delete(Currency toDelete) {
-        return null;
+        try {
+            connection = Connectiondb.getConnection();
+            String SQL = "DELETE FROM currency WHERE currency_id = ?";
+            pstmt = connection.prepareStatement(SQL);
+            pstmt.setLong(1, toDelete.getCurrencyId());
+            int rowsAffected = pstmt.executeUpdate();
+            if (rowsAffected > 0) {
+                return toDelete;
+            }
+            throw new RuntimeException("Failed to delete currency with ID: " + toDelete.getCurrencyId());
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            closeResources();
+        }
+    }
+
+    private void closeResources() {
+        try {
+            if (rs != null) {
+                rs.close();
+            }
+            if (pstmt != null) {
+                pstmt.close();
+            }
+            if (connection != null) {
+                connection.close();
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
